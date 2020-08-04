195039fe4f8707cbd61f31ef7b8b74b71b2ef649
rmudiga <raja.mudiganti@optum.com>
2020-08-04 17:07:05 -0500

    javalang first commit

---
+++ src/javalang/learn/basics/VarOperFlow.java
@@ -0,0 +1,10 @@
+package javalang.learn.basics;
+
+public class VarOperFlow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
